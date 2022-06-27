6864dcb06d621ca0a85af8eb53c9d94a5b2762a2
daolanfler <daolanfler@gmail.com>
2022-06-27 11:03:57 +0800

    chore: use commonjs instead of umd

--- rollup.config.js
+++ rollup.config.js
@@ -1,69 +1,68 @@
-import {defineConfig} from 'rollup'
-import {babel} from '@rollup/plugin-babel'
-import {nodeResolve} from '@rollup/plugin-node-resolve'
-import vue from 'rollup-plugin-vue'
-import cjs from '@rollup/plugin-commonjs'
-import replace from '@rollup/plugin-replace'
-import css from 'rollup-plugin-css-only'
-import CleanCSS from 'clean-css'
+import { defineConfig } from "rollup";
+import { babel } from "@rollup/plugin-babel";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import vue from "rollup-plugin-vue";
+import cjs from "@rollup/plugin-commonjs";
+import replace from "@rollup/plugin-replace";
+import css from "rollup-plugin-css-only";
+import CleanCSS from "clean-css";
 // import {uglify} from 'rollup-plugin-uglify'
-import fs from 'fs'
+import fs from "fs";
 
-const config = require('./package.json')
+const config = require("./package.json");
 
-const { name, version } = config
-const getOutputFilename = type => `dist/${name}.${type}.js`
+const { name, version } = config;
+const getOutputFilename = (type) => `dist/${name}.${type}.js`;
 
-export { name, getOutputFilename }
+export { name, getOutputFilename };
 
-export default defineConfig(
-
-  {
-    input: 'src/index.js',
-    external: ['element-ui/lib/tooltip', 'vue'],
-    plugins: [
-      nodeResolve({
-        mainFields: ['module', 'jsnext:main', 'main', 'browser'],
-        extensions: ['.vue', '.js']
-      }),
-      vue({
-        css: false
-      }),
-      css({
-        output(style) {
-          !fs.existsSync('dist') && fs.mkdirSync('dist')
-          fs.writeFileSync(`dist/${name}.css`, new CleanCSS().minify(style).styles)
-        }
-      }),
-      babel({
-        exclude: 'node_modules/**',
-        extensions: ['.js', '.jsx', '.es6', '.es', '.mjs', '.vue'],
-        babelHelpers: 'bundled'
-        
-      }),
-      cjs(),
-      // uglify(),
-      replace({
-        VERSION: JSON.stringify(version),
-        preventAssignment: true
-      }),
-    ],
-    output: [
-      {
-        exports: 'named',
-        name,
-        file: getOutputFilename('umd'),
-        format: 'umd'
+export default defineConfig({
+  input: "src/index.js",
+  external: ["element-ui/lib/tooltip", "vue"],
+  plugins: [
+    nodeResolve({
+      mainFields: ["module", "jsnext:main", "main", "browser"],
+      extensions: [".vue", ".js"],
+    }),
+    css({
+      output(style) {
+        !fs.existsSync("dist") && fs.mkdirSync("dist");
+        fs.writeFileSync(
+          `dist/${name}.css`,
+          new CleanCSS().minify(style).styles
+        );
       },
-      {
-        exports: 'named',
-        name,
-        file: getOutputFilename('esm'),
-        format: 'esm'
-      }
-    ],
-    watch: {
-      include: 'src/**'
-    }
-  }
-)
+    }),
+    vue({
+      css: false,
+    }),
+    babel({
+      exclude: "node_modules/**",
+      extensions: [".js", ".jsx", ".es6", ".es", ".mjs", ".vue"],
+      babelHelpers: "bundled",
+    }),
+    cjs(),
+    // uglify(),
+    replace({
+      VERSION: JSON.stringify(version),
+      preventAssignment: true,
+    }),
+  ],
+  output: [
+    {
+      exports: "named",
+      name,
+      file: getOutputFilename("common"),
+      format: "commonjs",
+    },
+    {
+      exports: "named",
+      name,
+      file: getOutputFilename("esm"),
+      format: "esm",
+    },
+  ],
+  watch: {
+    include: "src/**",
+  },
+});
